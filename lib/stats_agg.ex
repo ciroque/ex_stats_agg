d2852c6e2ddfd77af579d344eaf64791999917a9
ciroque <scalawagz@outlook.com>
2017-12-23 10:17:46 -0800

    Add basic record_function_duration implementation

--- lib/stats_agg.ex
+++ lib/stats_agg.ex
@@ -5,4 +5,30 @@ defmodule Ciroque.Monitoring.StatsAgg do
 
   use GenServer
 
+  require Logger
+
+  ## Public Interface
+
+  def record_function_duration(server, %{module: _module, function: _function, duration: _duration} = args) do
+    GenServer.cast(server, {:record_function_duration, args})
+  end
+
+  ## GenServer
+
+  def start_link() do
+    start_link(%{})
+  end
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  def handle_cast({:record_function_duration, %{module: _module, function: _function, duration: _duration} = args}, state) do
+    Logger.debug("#{__MODULE__}::#{inspect(__ENV__.function)}(#{inspect(args)})[#{inspect(state)}]")
+    {:noreply, state}
+  end
 end
--- test/stats_agg_test.exs
+++ test/stats_agg_test.exs
@@ -1,5 +1,37 @@
 defmodule Ciroque.Monitoring.StatsAggTest do
   use ExUnit.Case
-  doctest Ciroque.Monitoring.StatsAgg
 
+  alias Ciroque.Monitoring.StatsAgg
+  doctest StatsAgg
+
+  defp empty_state, do: %{}
+
+  defp function_duration_args do %{
+      module: __MODULE__,
+      function: "test/0",
+      duration: 1000
+    }
+  end
+
+  setup do
+    {:ok, pid} = StatsAgg.start_link()
+    %{server: pid}
+  end
+
+  test "start link with initial state" do
+    {:ok, pid} = StatsAgg.start_link(empty_state())
+    assert pid != nil
+  end
+
+  test "handles record function duration cast", %{server: server} do
+    :ok = GenServer.cast(server, {:record_function_duration, function_duration_args()})
+    new_state = :sys.get_state(server)
+    assert new_state == empty_state()
+  end
+
+  test "record_function_duration public api", %{server: server} do
+    StatsAgg.record_function_duration(server, function_duration_args())
+    new_state = :sys.get_state(server)
+    assert new_state == empty_state()
+  end
 end
